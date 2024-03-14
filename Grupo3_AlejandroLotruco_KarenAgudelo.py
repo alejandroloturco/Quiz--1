33c1c155237ba1d36801edd5ea471d9017c6c0ad
Karensity12 <karen.agudelot@udea.edu.co>
2024-03-13 20:48:06 -0500

    Creación de metodos get y set además se crea el metodo asignar revision.

--- Grupo3_AlejandroLotruco_KarenAgudelo.py
+++ Grupo3_AlejandroLotruco_KarenAgudelo.py
@@ -10,3 +10,41 @@ class Implantes:
     self.__revision = {} 
     self.__estado = estado
     self.__mantenimiento = ""
+
+  def verMaterial(self):
+    return self.__material
+  def verTamaño(self):
+    return self.__tamaño
+  def verMedico(self):
+    return self.__medico
+  def verEstado(self):
+    return self.__estado
+  def verRevision(self):
+    return self.__revision
+  def fecha_r(self):
+    return self.__fecha_r
+  def fecha_i(self):
+    return self.__fecha_i
+  def verMantenimiento(self):
+    return self.__mantenimiento
+
+  def asignarMaterial(self, m):
+    self.__material = m
+  def asignarTamaño(self, t):
+    self.__tamaño = t
+  def asignarMedico(self, med):
+    self.__tamaño = med
+  def asignarEstado(self, e):
+    self.__tamaño = e
+  def asignarFecha_r(self, r):
+    self.__fecha_r = r
+  def asignarRevision(self,sis2 ,fecha, manten):
+    if fecha in sis2:
+      self.__revision[fecha].append(manten)     
+    else:
+      self.__revision[fecha] = []
+      self.__revision[fecha].append(manten)
+    print("mantenimiento registrado exitosamente\n")
+
+  def asignarMantenimiento(self,m):
+    self.__mantenimiento = m
